3c5602552259dd540f41ef83ffee7a92bb3ba8e5
EnOte <164638951+EnOtese@users.noreply.github.com>
2025-01-10 16:29:28 +0300

    Add files via upload

---
+++ module19django1/task1/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer, Game
+
+
+# Create your views here.
+
+def platform(request):
+    return render(request, 'fourth_task/platform.html')
+
+
+def games(request):
+    games = Game.objects.all()
+    context = {
+        'games': games
+    }
+    return render(request, 'fourth_task/games.html', context)
+
+
+def cart(request):
+    return render(request, 'fourth_task/cart.html')
+
+users = Buyer.objects.all()
+
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if Buyer.objects.filter(name=username).exists():
+            info['error'] = 'Пользователь уже существует'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        else:
+            Buyer.objects.create(name=username, age=int(age), balance=0)
+            info['success'] = f'Приветствуем, {username}!'
+
+    info['form'] = UserRegister()
+    return render(request, 'fifth_task/registration_page.html', info)
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if username in users:
+            info['error'] = 'Пользователь уже существует'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        else:
+            info['success'] = f'Приветствуем, {username}!'
+
+    info['form'] = UserRegister()
+    return render(request, 'fifth_task/registration_page.html', info)
