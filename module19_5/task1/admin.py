9bf767b46477dd6b0f7147dd1d931f3007a55a9e
EnOte <164638951+EnOtese@users.noreply.github.com>
2025-01-11 17:39:33 +0400

    Add files via upload

---
+++ module19_5/task1/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from task1.models import Game, Buyer, News
+
+# Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('size', 'cost')
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title', )
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
+admin.site.register(News)
